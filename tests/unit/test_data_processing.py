e1316c4518dadf8dd75451027058e71d9f1f1100
Menegraal X <axelonobiono@gmail.com>
2025-03-20 21:33:37 +0100

    Adjusted the test_data_processing file for absolute path searching

--- tests/unit/test_data_processing.py
+++ tests/unit/test_data_processing.py
@@ -2,12 +2,18 @@ import pytest
 import pandas as pd
 import torch
 from unittest.mock import patch, MagicMock
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
+
 from src.data_extraction import load_data, to_sentiment, create_sentiment_column
 from src.data_processing import preprocess_reviews_processing, SentimentDataset, prepare_dataloader
 
 # Sample test data
 def sample_dataframe():
-    return load_data(path_to_data=r".\src\reviews.csv")
+    file_path = os.path.join(os.path.dirname(__file__), '../../src/reviews.csv')
+    return load_data(path_to_data=file_path)
 
 # Test BERT tokenization
 @patch("src.data_processing.tokenizer")
@@ -39,6 +45,7 @@ def test_sentiment_dataset():
 @patch("src.data_processing.create_sentiment_column_processing")
 @patch("src.data_processing.preprocess_reviews_processing")
 def test_prepare_dataloader(mock_preprocess, mock_create_sentiment, mock_load_data):
+    file_path = os.path.join(os.path.dirname(__file__), '../../src/reviews.csv')
     df = create_sentiment_column(sample_dataframe())
     mock_load_data.return_value = df
     mock_create_sentiment.return_value = df
@@ -47,9 +54,12 @@ def test_prepare_dataloader(mock_preprocess, mock_create_sentiment, mock_load_da
         torch.tensor([[1, 1, 1], [1, 1, 0], [1, 1, 1]]),
         torch.tensor([2, 1, 0])
     )
-    dataloader = prepare_dataloader(r".\src\reviews.csv", batch_size=2)
-    assert isinstance(dataloader, torch.utils.data.DataLoader)
-    batch = next(iter(dataloader))
+    train_dataloader, test_dataloader = prepare_dataloader(file_path, batch_size=2)
+    assert isinstance(train_dataloader, torch.utils.data.DataLoader)
+    assert isinstance(test_dataloader, torch.utils.data.DataLoader)
+    dataloader = train_dataloader, test_dataloader
+    assert isinstance(dataloader, tuple)
+    batch = next(iter(train_dataloader))
     assert "input_ids" in batch
     assert "attention_mask" in batch
     assert "labels" in batch
